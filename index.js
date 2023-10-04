aa9bcfe2133b5bf28cf0b7bfc57880dd679130dc
EsdrasPark <esdrasparkpark@gmail.com>
2023-10-03 21:36:09 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,46 @@
+let nome = "Esdras Park";
+let experiencia = 4500;
+
+if (experiencia === 999 || experiencia <= 999){
+    console.log("Ferro");
+}
+
+else if (experiencia >= 1000 && experiencia <= 1999){
+	console.log("Bronze");
+} 
+
+else if (experiencia >= 2000 && experiencia <= 2999){
+	console.log("Prata");
+} 
+
+else if (experiencia >= 3000 && experiencia <= 3999){	
+	console.log("Ouro");
+}
+
+else if (experiencia >= 4000 && experiencia <= 5999){
+	console.log("Platina");
+}
+
+else if (experiencia >= 6000 && experiencia <=8999){
+	console.log("Diamente");
+}    
+
+else if (experiencia >= 9000 && experiencia <= 14999){
+	console.log("Cosmico");
+}
+
+else if (experiencia >= 15000){
+	console.log("Challenger");
+}    
+
+
+console.log(nome + " Nível de XP " + experiencia)
+
+
+// considerações finais: Fiz alguma modificações no exemplo que o professor pediu para ficar do meu agrado
+
+// opitei por usar o (else if), 
+
+//tive bastante dificuldade precisei da ajuda de um amigo para lembrar de usar o ( || )
+
+// e precisei usar o chat GPT para lembrar de usar o ( && ) e ( ; )
